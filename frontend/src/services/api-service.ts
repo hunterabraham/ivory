d270246a6704fa67435f36727a9caf744bdad461
Hunter Abraham <hunterabraham@Hunters-Laptop.lan>
2024-12-20 22:36:58 -0500

    Add some stuff

--- frontend/src/services/api-service.ts
+++ frontend/src/services/api-service.ts
@@ -7,32 +7,4 @@ const instance = axios.create({
   },
 })
 
-instance.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response.status === 401) {
-      const refreshToken = localStorage.getItem("refresh_token")
-
-      localStorage.removeItem("access_token")
-      window.location.href = "/login"
-    }
-    return Promise.reject(error)
-  },
-)
-
-instance.interceptors.request.use(config => {
-  const token = localStorage.getItem("access_token")
-  const organization = localStorage.getItem("organization")
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-
-  if (organization) {
-    config.headers.Organization = organization
-  }
-
-  return config
-})
-
 export default instance
